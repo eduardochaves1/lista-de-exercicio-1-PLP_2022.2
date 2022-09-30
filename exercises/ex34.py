f1b266e74587cb23c1db4c953754e98c6974bf9c
Eduardo Chaves <henriqueduardo2002@gmail.com>
2022-09-30 02:08:36 -0300

    Improving ex34 part.2
    Now it is much more complex and error proof

--- exercises/ex34.py
+++ exercises/ex34.py
@@ -4,38 +4,77 @@
 # pessoas ou parar.//
 people = {}
 
+
+# register a person on the people dict
+def registerPerson (cpf):
+  name = input(f'+ Digite o nome da pessoa: ')
+  height = float(input(f'+ Digite a altura da pessoa [ex.: 1.67]: '))
+  weight = float(input(f'+ Digite o peso da pessoa [ex.: 52.3]: '))
+  sex = input(f'+ Digite o sexo da pessoa [f/m]: ')
+
+  imc = round(weight / height**2, 1) # the "1" after the calculus is to round the result with only one float digit
+
+  people.update({cpf : {"name": name, "height": height, "weight": weight, "sex": sex, "imc": imc}})
+
+
+# prints out the person's data from the people dict
+def printPersonData (cpfSearch):
+  personName = people[cpfSearch]["name"]
+  personHeight = people[cpfSearch]["height"]
+  personWeight = people[cpfSearch]["weight"]
+  personSex = people[cpfSearch]["sex"]
+  personIMC = people[cpfSearch]["imc"]
+
+  if personSex == "m" or personSex == "M":
+    personSex = "Másculino"
+  elif personSex == "f" or personSex == "F":
+    personSex = "Feminino"
+  else:
+    personSex = "Null"
+
+  print(f'- CPF: {cpfSearch} | Nome: {personName} | Altura: {personHeight} | Peso: {personWeight} | Sexo: {personSex} | IMC: {personIMC}')
+
+
 while True:
-  print('====== MENU ======')
+  print('\n====== MENU ======')
   print('1 - Cadastrar Pessoa')
-  print('2 - Pesquisar um Cadastro')
-  print('3 - Listar Dados de uma Pessoa')
-  print('4 - Parar')
-  menuChoice = int(input('\n ? Digite o número de sua escolha: '))
+  print('2 - Listar Dados de uma Pessoa')
+  print('3 - Parar')
+  menuChoice = int(input('\n? Digite o número de sua escolha: '))
 
   if menuChoice == 1:
-    # cadastrar uma pessoa no dicionário
-    name = input(f'\n + Digite o nome da pessoa: ')
-    height = float(input(f'+ Digite a altura da pessoa [ex.: 1.67]: '))
-    weight = float(input(f'+ Digite o peso da pessoa [ex.: 52.3]: '))
-    cpf = int(input(f'+ Digite o CPF da pessoa [somente números]: '))
-    sex = input(f'+ Digite o sexo da pessoa [f/m]: ')
+    cpf = int(input(f'\n+ Digite o CPF da pessoa [somente números]: '))
 
-    imc = weight / height**2
+    if cpf in people:
+      updateChoice = int(input('! Este CPF já foi cadastrado! Se desejas consultá-lo digite "1"; Se desejas alterá-lo digite "2"; Ou se desejas voltar digite "3": '))
+      if updateChoice == 1:
+        printPersonData(cpf)
+      elif updateChoice == 2:
+        registerPerson(cpf)
+    else:
+      registerPerson(cpf)
 
-    people.update({cpf : {"name": name, "height": height, "weight": weight, "sex": sex, "imc": imc}})
 
   elif menuChoice == 2:
-    # consultar uma pessoa no dicionário
-    cpfSearch = int(input('\n ? Digite o CPF da pessoa que desaja saber o IMC: '))
-    personName = people[cpfSearch]["name"]
-    personIMC = people[cpfSearch]["imc"]
-    print(f'- O IMC de {personName} (CPF: {cpfSearch}) é de {personIMC:.1f}')
+    cpfSearch = int(input('\n? Digite o CPF da pessoa que desaja consultar [somente números]: '))
 
-  elif menuChoice == 3:
-    # printar os dados de uma pessoa no dicionário
-    print(f'\n {people}')
+    while cpfSearch not in people:
+      errorChoice = int(input('! Este CPF não está cadastrado! Se desejas cadastrá-lo digite "1". Se desejas pesquisar por outro CPF digite "2". Ou se desejas voltar digite "3": '))
+      if errorChoice == 1:
+        registerPerson(cpfSearch)
+      elif errorChoice == 2:
+        cpfSearch = int(input('\n? Digite o CPF da pessoa que desaja consultar [somente números]: '))
+        if cpfSearch in people:
+          printPersonData(cpfSearch)
+          break
+      else:
+        break
+
+    else:
+      printPersonData(cpfSearch)
+
 
   else:
-    # finalizar o código
-    print('\n - Até a próxima!')
+    # ends the code
+    print('\n- Até a próxima!')
     break

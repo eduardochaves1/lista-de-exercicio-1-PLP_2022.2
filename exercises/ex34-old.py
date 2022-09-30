1fedb4381e7b348cbfacd870d58643316faa0ad6
Eduardo Chaves <95307858+eduardochaves1@users.noreply.github.com>
2022-09-29 21:59:37 -0300

    Improving ex34 part.1
    Making this commit from my laptop so I can pull from my desktop and keep working on that exercise

---
+++ exercises/ex34-old.py
@@ -0,0 +1,36 @@
+# 34) Faça um programa que //cadastre o **nome**, a **altura**, o **peso**, o **cpf** e **sexo**
+# de **algumas pessoas**//. Com os dados cadastrados, em seguida //localizar uma pessoas através
+# do seu CPF e imprimir o seu **IMC**//. Obs.: //Solicitar ao usuário se deseja cadastrar mais
+# pessoas ou parar.//
+people = {}
+
+personOrder = 0
+while True:
+  personOrder += 1
+  name = input(f'\n + [{personOrder}] Digite o nome da pessoa: ')
+  height = float(input(f'+ [{personOrder}] Digite a altura da pessoa [ex.: 1.67]: '))
+  weight = float(input(f'+ [{personOrder}] Digite o peso da pessoa [ex.: 52.3]: '))
+  cpf = int(input(f'+ [{personOrder}] Digite o CPF da pessoa [somente números]: '))
+  sex = input(f'+ [{personOrder}] Digite o sexo da pessoa [f/m]: ')
+
+  imc = weight / height**2
+
+  people.update({cpf : {"name": name, "height": height, "weight": weight, "sex": sex, "imc": imc}})
+
+  proceedChoice = input('\n ? Desejas cadastrar outra pessoa [s/n]? ')
+  if proceedChoice == 'n' or proceedChoice == 'N':
+    break
+
+while True:
+  cpfSearch = int(input('\n ? Digite o CPF da pessoa que desaja saber o IMC: '))
+  personName = people[cpfSearch]["name"]
+  personIMC = people[cpfSearch]["imc"]
+  print(f'- O IMC de {personName} (CPF: {cpfSearch}) é de {personIMC:.1f}')
+
+  proceedChoice = input('\n ? Desejas pesquisar sobre o IMC de outra pessoa [s/n]? ')
+  if proceedChoice == 'n' or proceedChoice == 'N':
+    print('- Até a próxima!')
+    break
+
+
+print(f'\n {people}')
--- exercises/ex34.py
+++ exercises/ex34.py
@@ -4,30 +4,38 @@
 # pessoas ou parar.//
 people = {}
 
-personOrder = 0
 while True:
-  personOrder += 1
-  name = input(f'\n + [{personOrder}] Digite o nome da pessoa: ')
-  height = float(input(f'+ [{personOrder}] Digite a altura da pessoa [ex.: 1.67]: '))
-  weight = float(input(f'+ [{personOrder}] Digite o peso da pessoa [ex.: 52.3]: '))
-  cpf = int(input(f'+ [{personOrder}] Digite o CPF da pessoa [somente números]: '))
-  sex = input(f'+ [{personOrder}] Digite o sexo da pessoa [f/m]: ')
+  print('====== MENU ======')
+  print('1 - Cadastrar Pessoa')
+  print('2 - Pesquisar um Cadastro')
+  print('3 - Listar Dados de uma Pessoa')
+  print('4 - Parar')
+  menuChoice = int(input('\n ? Digite o número de sua escolha: '))
 
-  imc = weight / height**2
+  if menuChoice == 1:
+    # cadastrar uma pessoa no dicionário
+    name = input(f'\n + Digite o nome da pessoa: ')
+    height = float(input(f'+ Digite a altura da pessoa [ex.: 1.67]: '))
+    weight = float(input(f'+ Digite o peso da pessoa [ex.: 52.3]: '))
+    cpf = int(input(f'+ Digite o CPF da pessoa [somente números]: '))
+    sex = input(f'+ Digite o sexo da pessoa [f/m]: ')
 
-  people.update({cpf : {"name": name, "height": height, "weight": weight, "sex": sex, "imc": imc}})
+    imc = weight / height**2
 
-  proceedChoice = input('\n ? Desejas cadastrar outra pessoa [s/n]? ')
-  if proceedChoice == 'n' or proceedChoice == 'N':
-    break
+    people.update({cpf : {"name": name, "height": height, "weight": weight, "sex": sex, "imc": imc}})
 
-while True:
-  cpfSearch = int(input('\n ? Digite o CPF da pessoa que desaja saber o IMC: '))
-  personName = people[cpfSearch]["name"]
-  personIMC = people[cpfSearch]["imc"]
-  print(f'- O IMC de {personName} (CPF: {cpfSearch}) é de {personIMC:.1f}')
+  elif menuChoice == 2:
+    # consultar uma pessoa no dicionário
+    cpfSearch = int(input('\n ? Digite o CPF da pessoa que desaja saber o IMC: '))
+    personName = people[cpfSearch]["name"]
+    personIMC = people[cpfSearch]["imc"]
+    print(f'- O IMC de {personName} (CPF: {cpfSearch}) é de {personIMC:.1f}')
 
-  proceedChoice = input('\n ? Desejas pesquisar sobre o IMC de outra pessoa [s/n]? ')
-  if proceedChoice == 'n' or proceedChoice == 'N':
-    print('- Até a próxima!')
+  elif menuChoice == 3:
+    # printar os dados de uma pessoa no dicionário
+    print(f'\n {people}')
+
+  else:
+    # finalizar o código
+    print('\n - Até a próxima!')
     break
